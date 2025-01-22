4883125adcd9655cd5706279c3fb60315012b50b
Darya Melgunova <darya.melgunova@gmail.com>
2025-01-22 18:43:37 +0300

    added jenkins cnfg

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,27 +1,51 @@
 package tests;
 import com.codeborne.selenide.Configuration;
-
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 
 public class TestBase {
     @BeforeAll
-    static void beforeAll() {
-        Configuration.browserSize = "1280x672";
-        Configuration.pageLoadStrategy = "eager";
+    static void setupConfig() {
+
         Configuration.baseUrl = "https://www.tbank.ru";
+        Configuration.browserSize = System.getProperty("browserSize", "1280x672");
+        Configuration.pageLoadStrategy = "eager";
+
+        String remoteHost = System.getProperty("remoteHost", "localhost");
+        Configuration.remote = "https://user1:1234@" + remoteHost + "/wd/hub";
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "120");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
     }
+
+
     @BeforeEach
-    void beforeEach() {
+    void setupTest() {
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
+
     @AfterEach
-    void closeWebDriver() {
+    void teardown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
         Selenide.closeWebDriver();
     }
 }
